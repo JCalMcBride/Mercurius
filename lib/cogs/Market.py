59c7166030b9ca22afbbb4dec67f94e5eb281cdb
Jacob McBride <jake55111@gmail.com>
2023-04-19 10:26:58 -0500

    Work on orders

--- lib/cogs/Market.py
+++ lib/cogs/Market.py
@@ -150,9 +150,9 @@ class MarketItem:
         volume = [x[0] for x in volume]
         print(volume)
         volume_string = ""
-        volume_string += f"Last 24 hours: ``{sum(volume[-1:])}``\n"
-        volume_string += f"Last 7 days: ``{sum(volume[-7:])}`` - ``{sum(volume[-7:]) // 7}``\n"
-        volume_string += f"Last {days} days: ``{sum(volume)}`` - ``{sum(volume) // days}``\n"
+        volume_string += f"**24 hour:** ``{sum(volume[-1:])}``\n"
+        volume_string += f"**7 day:** ``{sum(volume[-7:])}`` - ``{sum(volume[-7:]) // 7}``\n"
+        volume_string += f"**{days} day:** ``{sum(volume)}`` - ``{sum(volume) // days}``\n"
         return volume_string
 
     def __str__(self):
